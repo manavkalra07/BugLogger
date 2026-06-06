c5425292dd7772bcb89987b7e4f88ed4eaaa6bb2
Manav <iammanavkalra@email.com>
2026-06-06 18:31:35 +0530

    Setup MySQL database connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,25 @@
 const app = require("./app");
+const db = require("./config/db");
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log("server is running on port" + PORT)
-})
+
+async function startServer() {
+    try {
+        const connection = await db.getConnection();
+
+        console.log("Connected to the database");
+
+        connection.release();
+
+        app.listen(PORT, () => {
+            console.log("Server is running on port " + PORT);
+        });
+
+    } catch (error) {
+        console.log("Error connecting to the database");
+        console.error(error);
+
+        process.exit(1);
+    }
+}
+
+startServer();
